d95e7f2f2a3a8193dac12de41afc6d2153fc93cb
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-03-30 14:31:38 -0300

    Add files via upload

---
+++ ThreadTransacoes/src/controller/TransacoesController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class TransacoesController extends Thread{
+	private int id;
+	private int tipoTransacao;
+	private int permSaque;
+	private int permDeposito;
+	Semaphore semaforoSaque = new Semaphore(permSaque);
+	Semaphore semaforoDeposito = new Semaphore(permDeposito);
+	public TransacoesController(int id, int tipoTransacao, int permSaque, int permDeposito, Semaphore semaforoSaque, Semaphore semaforoDeposito) {
+		this.id = id;
+        this.tipoTransacao = tipoTransacao;
+        this.permSaque = permSaque;
+        this.permDeposito = permDeposito;
+        this.semaforoSaque = semaforoSaque;
+        this.semaforoDeposito = semaforoDeposito;  
+	}
+	@Override
+	public void run() {
+		if (tipoTransacao == 0) {
+            try {
+                semaforoSaque.acquire();
+                saque();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaforoSaque.release();
+            }
+        } else if (tipoTransacao == 1) {
+            try {
+                semaforoDeposito.acquire();
+                deposito();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaforoDeposito.release();
+            }
+        }
+	}
+	public void saque() {
+		        System.out.println("Cliente " + id + " sacando");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Cliente " + id + " terminou de sacar");
+    }
+    public void deposito() {
+        System.out.println("Cliente " + id + " depositando");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Cliente " + id + " terminou de depositar");
+	}
+}
---
+++ ThreadTransacoes/src/view/Main.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.util.concurrent.Semaphore;
+
+import controller.TransacoesController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int permSaque = 1;
+		int permDeposito = 1;
+		int tipoTransacao;
+		Semaphore semaforoSaque = new Semaphore(permSaque);
+		Semaphore semaforoDeposito = new Semaphore(permDeposito);
+		for (int id = 0; id < 20; id++) {
+            tipoTransacao = (int) (Math.random() * 2);
+            Thread thread = new TransacoesController(id, tipoTransacao, permSaque, permDeposito, semaforoSaque, semaforoDeposito);
+            thread.start();
+        }
+
+	}
+
+}
